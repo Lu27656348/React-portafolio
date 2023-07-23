ae2388ed2d2cb2349ad128055657cbcce56be043
Lu27656348 <luiscarlossomoza@gmail.com>
2023-07-23 17:45:24 -0400

    Tailwind configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    
+    <div className = "bg-slate-200 flex flex-box gap-4">
+      <div className = "bg-slate-600 basis-1/3 box-border h-32 w-32 flex justify-center items-center">
+        <h1>Hola Mundo</h1> 
+      </div>
+      <div className = "bg-slate-600 basis-1/3 box-border h-32 w-32 flex justify-center items-center">
+        <h1>Hola Mundo</h1> 
+      </div>
+      <div className = "bg-slate-600 basis-1/3 box-border h-32 w-32 flex justify-center items-center">
+        <h1>Hola Mundo</h1> 
+      </div>
+    </div>
+  )
+}
