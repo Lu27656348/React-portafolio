14bc962baa130dc1615f37d5c925ceca4a5a293e
Lu27656348 <luiscarlossomoza@gmail.com>
2023-07-26 21:48:32 -0400

    DotGroup component & menu-icon fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useMediaQuery from "./hooks/useMediaQuery.jsx";
 import Navbar from './scenes/Navbar.jsx';
+import DotGroup from './scenes/DotGroup.jsx';
+import Landing from './scenes/Landing.jsx';
 
 export default function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0 ){
+        setIsTopOfPage(true);
+      }else{
+        setIsTopOfPage(false)
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
   return (
     <div className = "app bg-deep-blue">
       <Navbar
+        isTopOfPage = {isTopOfPage}
         selectedPage = {selectedPage}
         setSelectedPage = {setSelectedPage}
       />
+      <div className = "w-5/6 mx-auto md:h-full" > 
+      {isAboveMediumScreens && (
+        <DotGroup 
+          selectedPage = {selectedPage}
+          setSelectedPage = {setIsTopOfPage}
+        />
+      )}
+      <Landing selectedPage = {selectedPage} />
+      </div>
     </div>
   )
 }
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery.jsx";
-const Link = ({page, selectedPage, setSelectedPage}) => {
+import menu_image from "../assets/icons8-menu.svg";
+import close_image from "../assets/icons8-close.svg"
+const Link = ({page, selectedPage, setSelectedPage,}) => {
     const lowerCasePage = page.toLowerCase();
     return ( 
         <AnchorLink className = {`${selectedPage === lowerCasePage ? "text-yellow" : "text-white"} hover:text-yellow transition duration-500`} href = {`#${lowerCasePage}`} onClick = {() => setSelectedPage(lowerCasePage)}>{page}</AnchorLink>
     )
 } 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage}) => {
 
     const [isMenuToggled, setIsMenuToggle] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-
+    const navbarBackground = isTopOfPage ? "" : "bg-red"
     return (
-        <nav className = "z-40 w-full fixed top-0 py-6">
+        <nav className = {` ${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className = "flex items-center justify-between mx-auto w-5/6">
                 <h4 className = "font-playfair text-3xl font-bold">LS</h4>
                 {}
@@ -27,7 +29,24 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
                     <Link page = "Contact" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
 
                 </div>) 
-                : (<button className = "rounded-full bg-red p-2" onClick = {()=>{setIsMenuToggle(!isMenuToggled)}}>Hola</button>)}
+                : (<button className = "rounded-full bg-red p-2" onClick = {()=>{setIsMenuToggle(!isMenuToggled)}}><img  alt = "icons8-menu" src = {menu_image} /></button>)}
+                {!isAboveSmallScreens && isMenuToggled && (
+                <div className = "fixed right-0 bottom-0 h-full bg-blue w-[300px] ">   
+                    <div className = "flex justify-end p-12">
+                        <button onClick = {()=>setIsMenuToggle(!isMenuToggled)}>
+                            <img  alt = "icons8-close" src = {close_image} />
+                        </button>
+
+                    </div>
+                    <div className = "flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                        <Link page = "Home" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
+                        <Link page = "Skills" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
+                        <Link page = "Projects" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
+                        <Link page = "Testimonials" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
+                        <Link page = "Contact" selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}></Link>
+
+                    </div>
+                </div>)}
             </div>
         </nav>
     )
