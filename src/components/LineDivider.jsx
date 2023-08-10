d05f0a11ab7dffc350df470e130c07c26ff337a8
Lu27656348 <luiscarlossomoza@gmail.com>
2023-08-09 20:22:35 -0400

    Changes in className of LineDivider

--- src/components/LineDivider.jsx
+++ src/components/LineDivider.jsx
@@ -1,5 +1,5 @@
 const LineDivider = ({ width = "w-full" }) => {
-    return <div className={`h-0.5 ${width} bg-red`} />;
+    return <div className={`h-0.5 ${width} bg-red mt-5 mb-5`} />;
   };
 
 export default LineDivider;
